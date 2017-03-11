0caa2e5b927016bba7e2ac6ee8367f0886e3473f
ValeriiSergheiev <Val.Serheiev@gmail.com>
2017-03-11 23:05:59 +0200

    main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -31,13 +31,61 @@ $(document).ready(function() {
 
 	//Slider
 	$('.slider-main').slick({
-		dots: true,
-		arrows: false
+		dots: false,
+		arrows: false,
+		autoplay: true
 	});
 
 	//Menu active item
 	$('.footwear-select-list li').click(function() {
 		$(this).addClass('active-menu-item').siblings().removeClass('active-menu-item');
+	});
+
+	//Footwear buttons / Auto height/width
+	var
+	footwear_btn_width = $('.footweat-type-btn').width(),
+	footwear_btn_height = footwear_btn_width * 0.84;
+
+	$('.footweat-type-btn').height(footwear_btn_height);
+
+	$(window).resize(function() {
+		footwear_btn_width = $('.footweat-type-btn').width();
+		footwear_btn_height = footwear_btn_width * 0.84;
+		$('.footweat-type-btn').height(footwear_btn_height);
+	});
+
+	//Custom Slideshow
+	//Auto height/width
+	var
+	display_width = $('.slideshow-display').width(),
+	item_width = (display_width - 30) / 3;
+
+	$('.slideshow-item').width(item_width);
+	$(window).resize(function() {
+		display_width = $('.slideshow-display').width();
+		item_width = (display_width - 30) / 3;
+		$('.slideshow-item').width(item_width);
+	});
+
+	//------------------
+	$('.slideshow-item a:first-child').click(function(event) {
+		event.preventDefault();
+	});
+	$('.slideshow-item a').click(function(event) {
+		//event.preventDefault();
+		var
+		path = $(this).find('img').attr('src'),
+		title = $(this).attr('title');
+		
+		if (!$(this).closest('.slideshow-item').hasClass('active')) {
+			$(this).closest('.slideshow-item').addClass('active').siblings().removeClass('active');
+			
+			$('.slideshow-display img').fadeOut(200, function() {
+				$(this).attr('src', path).fadeIn(200);
+				$(this).closest('a').attr('href', path);
+				$(this).closest('a').attr('title', title);
+			});
+		};
 	});
 
 });
